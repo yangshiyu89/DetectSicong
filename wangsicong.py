5d8a6b3603ed3293476e9e1cea301a0f4d86f524
Shiyu Yang <yangshiyu89@gmail.com>
2018-11-15 14:25:38 +0800

    Update wangsicong.py

--- wangsicong.py
+++ wangsicong.py
@@ -11,6 +11,7 @@ from matplotlib import pyplot as plt
 import timeit
 
 start = timeit.default_timer()
+
 img = cv2.imread('img/ducks.jpg')
 template = cv2.imread('img/wangsicong.jpg')
 template = cv2.resize(template, (36, 36), interpolation=cv2.INTER_CUBIC)
@@ -20,12 +21,17 @@ w, h = template[:,:,0].shape[::-1]
 
 # Apply Template Matching
 res = cv2.matchTemplate(img, template, eval('cv2.TM_CCOEFF'))
+
+# Get Anchor for the Template Matching Result
 min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+
+# Draw Rectangle
 top_left = max_loc
 bottom_right = (top_left[0] + w, top_left[1] + h)
 imgplt = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 cv2.rectangle(imgplt,top_left, bottom_right, 255, 2)
 
+# Show Image
 plt.imshow(imgplt)
 plt.title('Detected results'), plt.xticks([]), plt.yticks([])
 plt.show()
