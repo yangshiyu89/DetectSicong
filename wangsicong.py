31371d53b73d7071436751278eaaafec5565e2b7
Shiyu Yang <yangshiyu89@gmail.com>
2018-11-15 14:13:02 +0800

    Update wangsicong.py

--- wangsicong.py
+++ wangsicong.py
@@ -19,7 +19,7 @@ w, h = template[:,:,0].shape[::-1]
     
 
 # Apply template Matching
-res = cv2.matchTemplate(img,template,eval('cv2.TM_CCOEFF'))
+res = cv2.matchTemplate(img, template, eval('cv2.TM_CCOEFF'))
 min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
 top_left = max_loc
 bottom_right = (top_left[0] + w, top_left[1] + h)
